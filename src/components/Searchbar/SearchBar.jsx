152d00fe462afb888874980c2411ce1e68310ac9
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-09-09 23:33:26 +0300

    done

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -1,15 +1,47 @@
-<header class="searchbar">
-  <form class="form">
-    <button type="submit" class="button">
-      <span class="button-label">Search</span>
-    </button>
+import { Component } from "react";
+import { toast } from "react-toastify";
+import { FaSearch } from "react-icons/fa";
+import { Header, SearchForm, SearchInput, SearchBtn } from "./Searchbar.styled";
 
-    <input
-      class="input"
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
-    />
-  </form>
-</header>
+export default class Searchbar extends Component{
+  state = {
+    searchQuery: "",
+  }
+
+  handleSearchQueryChange = event => {
+    this.setState({
+      searchQuery: event.currentTarget.value.toLowerCase()
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.searchQuery.trim() === "" ) {
+      this.setState({ searchQuery: "" });
+      return toast.info("Please enter a search query");
+    }
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({ searchQuery: "" });
+  }
+
+  render() {
+    const { searchQuery } = this.state;
+    const { handleSubmit, handleSearchQueryChange } = this;
+
+    return <Header>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchInput
+            type="text"
+            autoComplete="off"
+            name="searchQuery"
+            placeholder="Search images and photos"
+            value={searchQuery}
+            onChange={handleSearchQueryChange}
+        />
+        <SearchBtn type="submit">
+          <FaSearch />
+        </SearchBtn>
+      </SearchForm>
+    </Header>
+  }
+}
